2375c38f24d6bbbbfc708f33b5b6a819cce12bee
Adam Harris <adamgrantharris14@gmail.com>
2017-11-14 21:39:13 -0600

    Landing page update

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-let name = anime({
-	targets: '#lineDrawing .lines path',
-	strokeDashoffset: [anime.setDashoffset, 0],
-	easing: 'easeInOutSine',
-	duration: 1000,
-	delay: function (el, i) {
-		return i * 250;
-	},
-	loop: true,
-	direction: 'alternate',
-});
+function svgNameAnimation() {
+	anime({
+		targets: '#svgContainer .lines path',
+		strokeDashoffset: [anime.setDashoffset, 0],
+		easing: 'easeInOutSine',
+		duration: 500,
+		delay: function (el, i) {
+			return i * 250;
+		},
+		complete: function () {
+			dotI();
+		}
+	});
+}
+
+/*function dotI() {
+	anime({
+		targets: '.nameI',
+		translateX: -10,
+		duration: 1000,
+		elasticity: 300,
+	});
+}*/
+
+svgNameAnimation();
